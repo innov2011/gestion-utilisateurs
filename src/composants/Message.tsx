99bfa816aa02e9978a93fd6f6adb65c3d0928398
Lewis DD <innov.contacts@gmail.com>
2026-09-09 04:39:39 +0100

    Exercices avec CSS sans Talwind ni Routes

---
+++ src/composants/Message.tsx
@@ -0,0 +1,44 @@
+type Params = {
+  index?: string;
+  picture?: string;
+  age?: number;
+  firstName?: string;
+  lastName?: string;
+  gender?: string;
+  email?: string;
+  phone?: string;
+  address?: string;
+  about?: string;
+};
+
+function Messsage(params: Params) {
+  return (
+    <article className="cards">
+      <div>
+        <img src={params?.picture ? params.picture : ""} alt="" />
+      </div>
+      <div className="info">
+        <p className="nomPrenom">
+          {params?.firstName ? <span>{params.firstName} </span> : "----"}{" "}
+          {params?.lastName ? (
+            <span>{params.lastName.toUpperCase()} </span>
+          ) : (
+            "----"
+          )}
+        </p>
+        <p className="autre">
+          Genre : {params?.gender ? <span>{params.gender} </span> : "----"} {"  "};
+          Age : {params?.age ? <span>{params.age} </span> : "----"} ans{" "}
+        </p>
+        <p className="autre">
+          phone : {params?.phone ? <span>{params.phone} </span> : "0000"}{" "}
+        </p>
+        <p className="autre">
+          Adress : {params?.address ? <span>{params.address} </span> : "----"}{" "}
+        </p>
+      </div>
+    </article>
+  );
+}
+
+export default Messsage;
---
+++ src/pages/LesDonnees.tsx
@@ -0,0 +1,49 @@
+import Messsage from "../composants/Message";
+
+type Message = {
+  index?: string;
+  picture?: string;
+  age?: number;
+  firstName?: string;
+  lastName?: string;
+  gender?: string;
+  email?: string;
+  phone?: string;
+  address?: string;
+  about?: string;
+};
+
+type Params = {
+  donnees: Message[];
+};
+
+function Donnees(params: Params) {
+  const donnees = params.donnees;
+  return (
+    <section className="grid ">
+      {" "}
+      {donnees.map(
+        ({
+          lastName,
+          firstName,
+          address,
+          phone,
+          gender,
+          age,
+          picture,
+        }: Message) => (
+          <Messsage
+            firstName={firstName}
+            lastName={lastName}
+            address={address}
+            phone={phone}
+            gender={gender}
+            age={age}
+            picture={picture}
+          />
+        ),
+      )}
+    </section>
+  );
+}
+export default Donnees;
